d466337a9ca208fb59657603fe686dd8883c8018
wtoshm <wtoshm@gmail.com>
2025-07-15 18:37:24 +0900

    feat: add 3 input field components

--- lib/presentation/components/input_fields/default_input_field.dart
+++ lib/presentation/components/input_fields/default_input_field.dart
@@ -42,6 +42,64 @@ class DefaultInputField extends StatelessWidget {
               //   borderSide:BorderSide(color: AppColors.gray4),
               //   borderRadius: BorderRadius.circular(10),
               // ),
+              // focusedBorder: OutlineInputBorder(
+              //   borderRadius: BorderRadius.circular(10),
+              //   borderSide: BorderSide(
+              //     color: AppColors.primary80,
+              //   ),
+              // ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Text(
+            label,
+            style: TextStyles.smallTextRegular,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          TextField(
+            onChanged: onValueChange,
+            decoration: InputDecoration(
+              hintText: placeHolder,
+              hintStyle: TextStyle(color: AppColors.gray4),
+              // enabledBorder: OutlineInputBorder(
+              //   borderSide:BorderSide(color: AppColors.gray4),
+              //   borderRadius: BorderRadius.circular(10),
+              // ),
+              // border: OutlineInputBorder(
+              //   borderSide:BorderSide(color: AppColors.gray4),
+              //   borderRadius: BorderRadius.circular(10),
+              // ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
+                borderSide: BorderSide(
+                  color: AppColors.primary80,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 20,),
+          Text(
+            label,
+            style: TextStyles.smallTextRegular,
+          ),
+          SizedBox(
+            height: 5,
+          ),
+          TextField(
+            onChanged: onValueChange,
+            decoration: InputDecoration(
+              hintText: placeHolder,
+              hintStyle: TextStyle(color: AppColors.gray4),
+              enabledBorder: OutlineInputBorder(
+                borderSide:BorderSide(color: AppColors.gray4),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              // border: OutlineInputBorder(
+              //   borderSide:BorderSide(color: AppColors.gray4),
+              //   borderRadius: BorderRadius.circular(10),
+              // ),
               focusedBorder: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(10),
                 borderSide: BorderSide(
