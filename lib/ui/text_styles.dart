3aaf9bf8862ddba3b0ba7ebfac94026aa1a7f015
wtoshm <wtoshm@gmail.com>
2025-07-15 15:44:35 +0900

    refactor: use constant for line height in text styles

--- lib/ui/text_styles.dart
+++ lib/ui/text_styles.dart
@@ -1,54 +1,56 @@
 import 'package:flutter/material.dart';
 
 abstract class TextStyles {
+  static const lineHeightRatio = 1.5;
+
   // bold
   static const TextStyle tittleTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 50,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle headerTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 30,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle largeTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 20,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle mediumTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 18,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle normalTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 16,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle smallTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 14,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle smallerTextBold = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.bold,
     fontSize: 11,
-    height: 1.5, // 17
+    height: lineHeightRatio, // 17
   );
 
   // regular
@@ -56,48 +58,48 @@ abstract class TextStyles {
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 50,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle headerTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 30,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle largeTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 20,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle mediumTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 18,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle normalTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 16,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle smallTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 14,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 
   static const TextStyle smallerTextRegular = TextStyle(
     fontFamily: 'Poppins',
     fontWeight: FontWeight.normal,
     fontSize: 11,
-    height: 1.5,
+    height: lineHeightRatio,
   );
 }
