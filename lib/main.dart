6baf43b62f3afb75fc10bd8368ccdb7e7891b464
wtoshm <wtoshm@gmail.com>
2025-07-15 21:14:45 +0900

    feat: add tabs widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_recipe_app/presentation/components/medium_button.dart';
-import 'package:flutter_recipe_app/presentation/components/small_button.dart';
+import 'package:flutter_recipe_app/presentation/components/buttons/big_button.dart';
+import 'package:flutter_recipe_app/presentation/components/input_fields/default_input_field.dart';
+import 'package:flutter_recipe_app/presentation/components/tabs/tabs.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +15,16 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(child: SmallButton(text: 'Button', onClick: () {})),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Tabs(labels: ['Label1', 'Label2'], selectedIndex: 0, onValueChange: (int value) {print(value);}),
+              SizedBox(height: 20),
+              Tabs(labels: ['Label1', 'Label2', 'Label3'], selectedIndex: 1, onValueChange: (int value) {print(value);}),
+            ],
+          ),
+        ),
       ),
     );
   }
---
+++ lib/presentation/components/tabs/tabs.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/ui/app_colors.dart';
+
+class Tabs extends StatelessWidget {
+  final List<String> labels;
+  final int selectedIndex;
+  final void Function(int value) onValueChange;
+
+  const Tabs({
+    super.key,
+    required this.labels,
+    required this.selectedIndex,
+    required this.onValueChange,
+  });
+
+  // final List<MyTab> myTabs = labels.map((e) => MyTab(tabLabel: e)).toList();
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: SizedBox(
+          width: double.infinity,
+          height: 58,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Row(
+              children: [
+                for (int i = 0; i < labels.length; i++)
+                  GestureDetector(
+                    onTap: () {
+                      onValueChange(i);
+                    },
+                    child: Container(
+                      width: labels.length < 3 ? 150 : 107,
+                      height: 33,
+                      alignment: Alignment.center,
+                      decoration: BoxDecoration(
+                        color: selectedIndex == i
+                            ? AppColors.primary100
+                            : Colors.transparent,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Text(
+                        labels[i],
+                        style: TextStyle(
+                          color: selectedIndex == i
+                              ? AppColors.white
+                              : AppColors.primary100,
+                        ),
+                      ),
+                    ),
+                  ),
+                // SizedBox(width: 10,),
+                // Expanded(
+                //   child: Container(
+                //     alignment: Alignment.center,
+                //     decoration: BoxDecoration(
+                //         color: selectedIndex == 1 ? AppColors.primary100 : Colors.transparent,
+                //         borderRadius: BorderRadius.circular(10)),
+                //     child: Text(labels[1], style: TextStyle(color: selectedIndex == 1 ? AppColors.white : AppColors.primary100),),
+                //   ),
+                // ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
