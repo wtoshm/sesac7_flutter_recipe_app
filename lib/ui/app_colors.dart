11f1c0e0702446198bbcffe8f3d0f1d3a8d91703
wtoshm <wtoshm@gmail.com>
2025-07-15 15:21:34 +0900

    feat: add app color palette

---
+++ lib/ui/app_colors.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // Netural Colors
+  static const Color black = Color(0xFF000000);
+  static const Color gray1 = Color(0xFF484848);
+  static const Color gray2 = Color(0xFF797979);
+  static const Color gray3 = Color(0xFFA9A9A9);
+  static const Color gray4 = Color(0xFFD9D9D9);
+  static const Color white = Color(0xFFFFFFFF);
+
+  // Primary Colors
+  static const Color primary100 = Color(0xFF129575);
+  static const Color primary80 = Color(0xFF71B1A1);
+  static const Color primary60 = Color(0xFFAFD3CA);
+  static const Color primary40 = Color(0xFFDBEBE7);
+  static const Color primary20 = Color(0xFFF6FAF9);
+
+  // Secondary Colors
+  static const Color secondary100 = Color(0xFFFF9C00);
+  static const Color secondary80 = Color(0xFFFFA61A);
+  static const Color secondary60 = Color(0xFFFFBA4D);
+  static const Color secondary40 = Color(0xFFFFCEB0);
+  static const Color secondary20 = Color(0xFFFFE1B3);
+
+  // Rating
+  static const Color rating = Color(0xFFFFAD30);
+
+  // Warning
+  static const Color warningRed = Color(0xFFE94A59);
+  static const Color warningPink = Color(0xFFFFE1E7);
+
+  // Success
+  static const Color success = Color(0xFF31B057);
+}
