cf6d4ecb613d2a76275425824995ebf29c7364fc
wtoshm <wtoshm@gmail.com>
2025-07-22 21:18:33 +0900

    feat: add GoRouter configuration

---
+++ lib/core/routing/router.dart
@@ -0,0 +1,97 @@
+// GoRouter configuration
+import 'package:flutter/material.dart';
+import 'package:flutter_recipe_app/core/routing/routes.dart';
+import 'package:flutter_recipe_app/data/data_source/recipe_data_source.dart';
+import 'package:flutter_recipe_app/data/data_source/recipe_data_source_impl.dart';
+import 'package:flutter_recipe_app/data/recipe_repository/recipe_repository.dart';
+import 'package:flutter_recipe_app/data/recipe_repository/recipe_repository_impl.dart';
+import 'package:flutter_recipe_app/presentation/screens/auth/sign_up_screen.dart';
+import 'package:flutter_recipe_app/presentation/screens/home/home_screen.dart';
+import 'package:flutter_recipe_app/presentation/screens/notifications/notifications_screen.dart';
+import 'package:flutter_recipe_app/presentation/screens/profile/profile_screeen.dart';
+import 'package:flutter_recipe_app/presentation/screens/saved_recipes/saved_recipes_screen.dart';
+import 'package:flutter_recipe_app/presentation/screens/saved_recipes/saved_recipes_view_model.dart';
+import 'package:flutter_recipe_app/presentation/screens/splash_screen.dart';
+import 'package:go_router/go_router.dart';
+import 'package:http/http.dart' as http;
+
+import '../../presentation/screens/auth/sign_in_screen.dart';
+
+final RecipeDataSource recipeDataSource = RecipeDataSourceImpl();
+final RecipeRepository recipeRepository = RecipeRepositoryImpl(
+  recipeDataSource,
+);
+
+final GoRouter router = GoRouter(
+  routes: [
+    // initialLocation: Routes.splash,
+    GoRoute(
+      path: Routes.splash,
+      builder: (context, state) => SplashScreen(
+        onTapStartCooking: () => context.go(Routes.signIn),
+      ),
+    ),
+    GoRoute(
+      path: Routes.signIn,
+      builder: (BuildContext context, GoRouterState state) => SignInScreen(
+        onTapSignIn: () => context.go(Routes.home),
+        onTapSignUp: () => context.go(Routes.signUp),
+      ),
+    ),
+    GoRoute(
+      path: Routes.signUp,
+      builder: (BuildContext context, GoRouterState state) => SignUpScreen(
+        onTapSignIn: () => context.go(Routes.signIn),
+      ),
+    ),
+    StatefulShellRoute.indexedStack(
+      branches: [
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.home,
+              builder: (context, state) => HomeScreen(),
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.savedRecipes,
+              builder: (context, state) {
+                final SavedRecipesViewModel savedRecipeViewModel =
+                    SavedRecipesViewModel(recipeRepository);
+                return ListenableBuilder(
+                  listenable: savedRecipeViewModel,
+                  builder: (context, child) {
+                    return SavedRecipesScreen(viewModel: savedRecipeViewModel);
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.notifications,
+              builder: (context, state) {
+                return const NotificationsScreen();
+              },
+            ),
+          ],
+        ),
+        StatefulShellBranch(
+          routes: [
+            GoRoute(
+              path: Routes.profile,
+              builder: (context, state) {
+                return const ProfileScreen();
+              },
+            ),
+          ],
+        ),
+      ],
+    ),
+  ],
+);
