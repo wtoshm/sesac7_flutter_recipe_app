f53dd4d24e8b5360bf1bd7f9f45ea45c470b5f88
wtoshm <wtoshm@gmail.com>
2025-07-22 17:28:07 +0900

    feat: define app routes

---
+++ lib/core/routing/routes.dart
@@ -0,0 +1,14 @@
+abstract class Routes {
+  // Splash screen route
+  static const String splash = '/splash';
+
+  // Authentication routes
+  static const String signIn = '/sign-in';
+  static const String signUp = '/sign-up';
+
+  // Main tap routes
+  static const String home = '/home';
+  static const String savedRecipes = '/saved-recipes';
+  static const String notifications = '/notifications';
+  static const String profile = '/profile';
+}
