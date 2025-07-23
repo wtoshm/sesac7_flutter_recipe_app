ae11b61609a94b61aeeb8c3383b182935dde973a
wtoshm <wtoshm@gmail.com>
2025-07-23 20:43:31 +0900

    feat: add mock ingredients

---
+++ lib/data/mock_data/mock_ingredients.dart
@@ -0,0 +1,65 @@
+import '../model/ingredient.dart';
+
+final mock_ingredients = [
+  Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),
+  Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),
+  Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),
+  Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),
+  Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),
+  Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),
+  Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),Ingredient(
+    title: 'Tomatos',
+    imageUrl:
+    'https://cdn.pixabay.com/photo/2017/10/06/17/17/tomato-2823826_1280.jpg',
+    quantity: 500,
+  ),
+
+
+
+
+];
