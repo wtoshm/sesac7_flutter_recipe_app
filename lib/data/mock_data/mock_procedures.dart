2f5d715b8e6f75b46f42aa75b5816d12b2f0f352
wtoshm <wtoshm@gmail.com>
2025-07-23 21:18:22 +0900

    feat: add mock procedures data

---
+++ lib/data/mock_data/mock_procedures.dart
@@ -0,0 +1,8 @@
+final mock_procedures = [
+  'Lorem ipsum tempor indicnent tu tjjfjj dkjfd kdje jjdjjs sss fjjjj fslfj  dkjfd kdje jjdjjs sss fjjjj fs',
+  'Lorem ipsum tempor indicnent tu tjjfjj dkjfd kdje jjdjjs sss fjjjj fslfj Lorem ipsum tempor indicnent tu tjjfjj dkjfd kdje jjdjjs sss fjjjj fslfj',
+  'Lorem ipsum tempor indicnent tu tjjfjj dkjfd kdje jjdjjs sss fjjjj fslfj',
+  'Lorem ipsum tempor indicnent tu tjjfjj dkjfd kdje jjdjjs sss fjjjj fslfj dfdfsdf dfdfdf kjfd kdje jjdjjs sss fjjjj fsl',
+  'Lorem ipsum tempor indicnent tu tjjfjj dkjfd kdje jjdjjs sss fjjjj fslfj',
+  'Lorem ipsum tempor indicnent tu tjjfjj dkjfd kdje jjdjjs sss fjjjj fslfj',
+];
